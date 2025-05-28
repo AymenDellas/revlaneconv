aebcd067fe23a2a24c950b19af221311aabbbf82
AymenDellas <aymen.dellas@etu.usthb.dz>
2025-05-28 20:30:03 +0100

    -------

---
+++ next.config.js
@@ -0,0 +1,21 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  webpack: (config, { isServer }) => {
+    // For server-side bundles, ignore .map files from chrome-aws-lambda
+    if (isServer) {
+      config.module.rules.push({
+        test: /\.map$/,
+        include: /node_modules\/chrome-aws-lambda/,
+        use: 'null-loader',
+      });
+    }
+
+    // Important: return the modified config
+    return config;
+  },
+  // If you have other Next.js configurations, they would go here
+  // For example:
+  // reactStrictMode: true,
+};
+
+module.exports = nextConfig;
